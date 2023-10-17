24f289ff7a9ae12b664fa6463ebf3f5dc5d39934
tedirland <irlandth@gmail.com>
2023-10-17 13:51:49 -0500

    added boilerplate pygame code

--- game.py
+++ game.py
@@ -1,4 +1,21 @@
 import pygame
+clock = pygame.time.Clock()
 
+pygame.init()
 
+#get the display info. Info is a class...
+display_info = pygame.display.Info()
+screen_size = (display_info.current_w, display_info.current_h)
+screen = pygame.display.set_mode(screen_size)
+RUNNING = True # BOOL for game loop
 
+# This is the main game loop
+while RUNNING:
+    # check to see if any events occured since the last time through the game loop
+    for event in pygame.event.get():
+        # Check to see what event it was
+        if event.type == pygame.QUIT:
+            RUNNING = False
+    screen.fill("red")
+    clock.tick(60) #integer passed is the FPS
+    pygame.display.flip() # DRAW OUR STUFF
