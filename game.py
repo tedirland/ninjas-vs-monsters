3d87bbf9bcd43a19d56581404d0be74fe2cadfb8
tedirland <irlandth@gmail.com>
2023-10-17 13:58:37 -0500

    display background on screen

--- game.py
+++ game.py
@@ -16,6 +16,16 @@ while RUNNING:
         # Check to see what event it was
         if event.type == pygame.QUIT:
             RUNNING = False
-    screen.fill("red")
+    # get image off of hd and load into pygame. Returns a Surface object
+    background_image = pygame.image.load("./images/background4.png")
+    # call the get_rect method on the Surface object
+    background_rect = background_image.get_rect()
+    # debug
+    print(background_rect)
+    # blit = block image transfer
+    # takes 2 args
+    # 1. What to draw
+    # 2. where to draw it
+    screen.blit(background_image, background_rect)
     clock.tick(60) #integer passed is the FPS
     pygame.display.flip() # DRAW OUR STUFF
